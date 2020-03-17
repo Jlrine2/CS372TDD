2c83886a265d7d97a9b53d26a90407fa3cf13406
James Rine <jlrine2@alaska.edu>
2020-03-17 14:28:33 -0800

    test one number string

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -1,4 +1,11 @@
 from string_calculator import add
 
+
 def test_empty_string_returns_0():
     assert add('') == 0
+
+
+def test_string_with_one_number():
+    assert add('2') == 2
+    assert add('6') == 6
+    assert add('58') == 58
