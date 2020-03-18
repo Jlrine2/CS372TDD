8d1a5c73c97129bef55639f03cc9a2ad3255fa1e
James Rine <jlrine2@alaska.edu>
2020-03-17 17:11:10 -0800

    add test for newline delimeter, already passes. consider tighening restrictions

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -14,3 +14,13 @@ def test_string_two_numbers():
     assert add('1,1') == 2
     assert add('8,6') == 14
     assert add('9,3') == 12
+
+def test_string_many_numbers():
+    assert add('1,2,81') == 84
+    assert add('8,9,6') == 23
+    assert add('0,0,3,9,9,3') == 24
+
+def test_newline_delimeter():
+    assert add('1,2,81') == 84
+    assert add('8\n9\n6') == 23
+    assert add('0\n0,3,9\n9,3') == 24
