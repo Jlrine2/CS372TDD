e23f2365a512d30d961282da047f05c8b164c3d5
James Rine <jlrine2@alaska.edu>
2020-03-17 16:50:05 -0800

    implemented 2 number string with regex

--- string_calculator.py
+++ string_calculator.py
@@ -1,8 +1,12 @@
 # string calculator field
 # James Rine
+from re import match
 
 
 def add(comma_delimited_numbers):
     if not comma_delimited_numbers:
         return 0
-    return int(comma_delimited_numbers)
+    num_list_match = match(r'(?P<first_num>\d+)(,(?P<second_num>\d+))?', comma_delimited_numbers)
+    if not num_list_match.group('second_num'):
+        return int(num_list_match.group('first_num'))
+    return int(num_list_match.group('first_num')) + int(num_list_match.group('second_num'))
