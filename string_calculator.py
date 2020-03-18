8c69c80d2593a19ce5a135e23042a765e0906e45
James Rine <jlrine2@alaska.edu>
2020-03-17 16:37:52 -0800

    implement one number string retruns the number

--- string_calculator.py
+++ string_calculator.py
@@ -1,6 +1,8 @@
-# string calculator fiel
+# string calculator field
 # James Rine
 
 
 def add(comma_delimited_numbers):
-    return 0
+    if not comma_delimited_numbers:
+        return 0
+    return int(comma_delimited_numbers)
