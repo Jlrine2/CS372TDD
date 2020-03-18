285182e5a654fb221bebbefe12537b4ef2bcd161
James Rine <jlrine2@alaska.edu>
2020-03-17 16:39:37 -0800

    test two number string

--- test_string_calculator.py
+++ test_string_calculator.py
@@ -9,3 +9,8 @@ def test_string_with_one_number():
     assert add('2') == 2
     assert add('6') == 6
     assert add('58') == 58
+
+def test_string_two_numbers():
+    assert add('1,1') == 2
+    assert add('8,6') == 14
+    assert add('9,3') == 12
